435fb86785139218e9f400a6311141b2f15fcea9
aa.semenenko <deskri@mail.ru>
2023-09-06 20:46:46 +0400

    Conection Identity and Entity Framework

---
+++ IS_FHGMOABO/DBConection/ApplicationDBContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+
+namespace IS_FHGMOABO.DBConection
+{
+    public class ApplicationDBContext : IdentityDbContext<IdentityUser>
+    {
+        public ApplicationDBContext(DbContextOptions<ApplicationDBContext> options) : base(options)
+        {
+
+        }
+    }
+}
