59fa2debc60c287fb664875c6b9e365ac4472a27
aa.semenenko <deskri@mail.ru>
2023-09-07 13:28:16 +0400

    Add controller and view of login form

---
+++ IS_FHGMOABO/Models/AccountsModels/LoginModel.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+
+namespace IS_FHGMOABO.Models.AccountsModels
+{
+    public class LoginModel
+    {
+        [Required(ErrorMessage = "Требуется ввести имя пользователя")]
+        [Display(Name = "Имя пользователя")]
+        public string UserName { get; set; }
+
+        [Required(ErrorMessage = "Необходимо ввести пароль")]
+        [Display(Name = "Пароль")]
+        public string Password { get; set; }
+
+        [Display(Name = "Запомнить меня")]
+        public bool RememberMe { get; set; }
+    }
+}
---
+++ IS_FHGMOABO/Models/AccountsModels/RegisterModel.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace IS_FHGMOABO.Models.AccountsModels
+{
+    public class RegisterModel
+    {
+        [Required(ErrorMessage = "Требуется ввести имя пользователя")]
+        public string UserName { get; set; }
+
+        [Required(ErrorMessage = "Необходимо ввести пароль")]
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
+
+        [Required(ErrorMessage = "Требуется ввести подтверждение пароля")]
+        [DataType(DataType.Password)]
+        [Compare("Password", ErrorMessage = "Пароли не совпадают")]
+        public string ConfirmPassword { get; set; }
+    }
+}
