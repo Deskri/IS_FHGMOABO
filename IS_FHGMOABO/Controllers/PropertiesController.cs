74d0b0b4fd9306d19f692b3127761c91c0c8eb00
aa.semenenko <deskri@mail.ru>
2023-09-19 14:07:28 +0400

    Add partial view to all add properties

--- IS_FHGMOABO/Controllers/PropertiesController.cs
+++ IS_FHGMOABO/Controllers/PropertiesController.cs
@@ -1,15 +1,55 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
 
 namespace IS_FHGMOABO.Controllers
 {
     public class PropertiesController : Controller
     {
+        [Authorize]
+        [HttpGet]
         public IActionResult Index()
         {
             return View();
         }
 
+        [Authorize]
+        [HttpGet]
         public IActionResult Add()
+        {
+            return PartialView();
+        }
+
+        [Authorize]
+        [HttpGet]
+        public IActionResult AddFullPropertyNaturalPerson() 
+        {
+            return PartialView();
+        }
+
+        [Authorize]
+        [HttpGet]
+        public IActionResult AddFullPropertyLegalPerson()
+        {
+            return PartialView();
+        }
+
+        [Authorize]
+        [HttpGet]
+        public IActionResult AddSharedPropertyNaturalPerson()
+        {
+            return PartialView();
+        }
+
+        [Authorize]
+        [HttpGet]
+        public IActionResult AddSharedPropertyLegalPerson()
+        {
+            return PartialView();
+        }
+
+        [Authorize]
+        [HttpGet]
+        public IActionResult AddJointProperty()
         {
             return PartialView();
         }
