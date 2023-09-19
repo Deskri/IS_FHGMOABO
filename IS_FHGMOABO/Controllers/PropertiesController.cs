7f576dc6bcc171aef3bdcef77a44349c71aa2646
aa.semenenko <deskri@mail.ru>
2023-09-19 13:13:04 +0400

    Create tamplate add property

---
+++ IS_FHGMOABO/Controllers/PropertiesController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace IS_FHGMOABO.Controllers
+{
+    public class PropertiesController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Add()
+        {
+            return PartialView();
+        }
+    }
+}
