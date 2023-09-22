09072f1c6a6f97353ada89cbf039f75bac1caed9
aa.semenenko <deskri@mail.ru>
2023-09-22 11:53:18 +0400

    Add new models to add

--- IS_FHGMOABO/Models/PropertiesModels/AddPropertiesModel.cs
+++ IS_FHGMOABO/Models/PropertiesModels/AddPropertiesModel.cs
@@ -5,17 +5,44 @@ namespace IS_FHGMOABO.Models.PropertiesModels
     public class AddPropertiesModel
     {
         public AddFullPropertyModel AddFullProperty { get; set; }
+
+        public AddSharedPropertyModel AddSharedProperty { get; set; }
+
+        public AddJointPropertyModel AddJointProperty {  get; set; } 
     }
 
-    public class AddFullPropertyModel
+    public class AddFullPropertyModel : AddProperty
     {
         public bool IsNaturalPerson { get; set; }
-
-        public string? RoomId { get; set; }
 
         public LegalPerson? LegalPerson { get; set; }
 
         public NaturalPerson? NaturalPerson { get; set; }
+    }
+
+    public class AddSharedPropertyModel : AddProperty
+    {
+        public bool IsNaturalPerson { get; set; }
+
+        public LegalPerson? LegalPerson { get; set; }
+
+        public NaturalPerson? NaturalPerson { get; set; }
+
+        public Share? Share { get; set; }
+    }
+
+    public class AddJointPropertyModel : AddProperty
+    {
+        public LegalPerson? FirstPerson { get; set; }
+
+        public LegalPerson? SecondPerson { get; set; }
+
+        public Share? Share { get; set; }
+    }
+
+    public class AddProperty
+    {
+        public string? RoomId { get; set; }
 
         public PropertyDate? PropertyDate { get; set; }
 
