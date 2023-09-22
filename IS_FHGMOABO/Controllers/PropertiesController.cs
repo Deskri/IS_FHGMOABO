cf499449c0ba3b8f51f306db773b6f1317f3a6b7
aa.semenenko <deskri@mail.ru>
2023-09-22 22:30:40 +0400

    Add post method addJointProperty

--- IS_FHGMOABO/Controllers/PropertiesController.cs
+++ IS_FHGMOABO/Controllers/PropertiesController.cs
@@ -243,23 +243,23 @@ namespace IS_FHGMOABO.Controllers
 
                 decimal share = 0;
 
-                if (_add.Share.Dividend == null 
+                if (_add.Share.Dividend == null
                     || _add.Share.Dividend <= 0)
                 {
                     ModelState.AddModelError("Share.Dividend", "Делимое доли должно быть внесено и не должно быть равно или меньше 0.");
                 }
-                if (_add.Share.Divisor == null 
+                if (_add.Share.Divisor == null
                     || _add.Share.Divisor <= 0)
                 {
                     ModelState.AddModelError("Share.Divisor", "Делитель доли должен быть внесен и не должен быть равен или меньше 0.");
                 }
-                if (_add.Share.Dividend != null 
+                if (_add.Share.Dividend != null
                     && _add.Share.Divisor != null
                     && _add.Share.Dividend > 0
                     && _add.Share.Divisor > 0)
                 {
                     share = (decimal)_add.Share.Dividend / _add.Share.Divisor;
-                    
+
                     if (share > 1)
                     {
                         ModelState.AddModelError("Share", "Делимое не может быть больше делителя.");
@@ -270,8 +270,8 @@ namespace IS_FHGMOABO.Controllers
                 {
                     ModelState.AddModelError("RoomId", "Помещение в системе не найдено.");
                 }
-                else if (_add.Share.Dividend != null 
-                    && _add.Share.Divisor != null 
+                else if (_add.Share.Dividend != null
+                    && _add.Share.Divisor != null
                     && _add.Share.Dividend > 0
                     && _add.Share.Divisor > 0
                     && share <= 1)
@@ -435,6 +435,185 @@ namespace IS_FHGMOABO.Controllers
         public IActionResult AddJointProperty()
         {
             return PartialView();
+        }
+
+        [Authorize]
+        [HttpPost]
+        public async Task<IActionResult> AddJointProperty(AddJointPropertyModel _add)
+        {
+            try
+            {
+                var _roomId = Int32.Parse(_add.RoomId);
+
+                var rooms = await _applicationDBContext.Rooms.FirstOrDefaultAsync(x => x.Id == _roomId && x.Deleted == null);
+
+                decimal share = 0;
+
+                if (_add.Share.Dividend == null
+                    || _add.Share.Dividend <= 0)
+                {
+                    ModelState.AddModelError("Share.Dividend", "Делимое доли должно быть внесено и не должно быть равно или меньше 0.");
+                }
+                if (_add.Share.Divisor == null
+                    || _add.Share.Divisor <= 0)
+                {
+                    ModelState.AddModelError("Share.Divisor", "Делитель доли должен быть внесен и не должен быть равен или меньше 0.");
+                }
+                if (_add.Share.Dividend != null
+                    && _add.Share.Divisor != null
+                    && _add.Share.Dividend > 0
+                    && _add.Share.Divisor > 0)
+                {
+                    share = (decimal)_add.Share.Dividend / _add.Share.Divisor;
+
+                    if (share > 1)
+                    {
+                        ModelState.AddModelError("Share", "Делимое не может быть больше делителя.");
+                    }
+                }
+
+                if (rooms == null)
+                {
+                    ModelState.AddModelError("RoomId", "Помещение в системе не найдено.");
+                }
+                else if (_add.Share.Dividend != null
+                    && _add.Share.Divisor != null
+                    && _add.Share.Dividend > 0
+                    && _add.Share.Divisor > 0
+                    && share <= 1)
+                {
+                    var properties = await _applicationDBContext.Properties.Where(x => x.RoomId == _roomId
+                                                                                && x.EndDate == null)
+                                                                                .ToListAsync();
+
+                    decimal shareAllRooms = 0;
+
+                    foreach (var property in properties)
+                    {
+                        shareAllRooms += property.Share;
+                    }
+
+                    if (shareAllRooms + share > 1)
+                    {
+                        ModelState.AddModelError("Rooms", "В этом помещение уже есть право собственности или сумма долей превышает единицу.");
+                    }
+                }
+
+                if (_add.PropertyDate.DateOfTaking > DateTime.Now)
+                {
+                    ModelState.AddModelError("PropertyDate.DateOfTaking", "Дата вступления в собственность не должна быть больше текущего времени.");
+                }
+
+                if (_add.FirstPerson.FirstName == null)
+                {
+                    ModelState.AddModelError("FirstPerson.FirstName", "Имя собственника должно быть заполнено.");
+                }
+                if (_add.FirstPerson.LastName == null)
+                {
+                    ModelState.AddModelError("FirstPerson.LastName", "Фамилия собственника должна быть заполнена.");
+                }
+                if (_add.FirstPerson.DateOfBirth > DateTime.Now)
+                {
+                    ModelState.AddModelError("FirstPerson.DateOfBirth", "Дата рождения не должна быть больше текущего времени.");
+                }
+
+                if (_add.SecondPerson.FirstName == null)
+                {
+                    ModelState.AddModelError("SecondPerson.FirstName", "Имя собственника должно быть заполнено.");
+                }
+                if (_add.SecondPerson.LastName == null)
+                {
+                    ModelState.AddModelError("SecondPerson.LastName", "Фамилия собственника должна быть заполнена.");
+                }
+                if (_add.SecondPerson.DateOfBirth > DateTime.Now)
+                {
+                    ModelState.AddModelError("SecondPerson.DateOfBirth", "Дата рождения не должна быть больше текущего времени.");
+                }
+
+                if (ModelState.IsValid)
+                {
+                    var property = new Property()
+                    {
+                        RoomId = _roomId,
+                        Type = "Совместная собственность",
+                        Share = (decimal)_add.Share.Dividend / _add.Share.Divisor,
+                        DateOfTaking = _add.PropertyDate.DateOfTaking,
+                        EndDate = _add.PropertyDate.EndDate,
+                        TypeOfStateRegistration = _add.StateRegistration.Type,
+                        StateRegistrationNumber = _add.StateRegistration.Number,
+                        ByWhomIssued = _add.StateRegistration.ByWhomIssued,
+
+                        NaturalPersons = new List<DAL.NaturalPerson>()
+                    };
+
+                    var firstPerson = new DAL.NaturalPerson()
+                    {
+                        FirstName = _add.FirstPerson.FirstName,
+                        LastName = _add.FirstPerson.LastName,
+                        Patronymic = _add.FirstPerson.Patronymic,
+                        DateOfBirth = _add.FirstPerson.DateOfBirth,
+                    };
+
+                    var secondPerson = new DAL.NaturalPerson()
+                    {
+                        FirstName = _add.SecondPerson.FirstName,
+                        LastName = _add.SecondPerson.LastName,
+                        Patronymic = _add.SecondPerson.Patronymic,
+                        DateOfBirth = _add.SecondPerson.DateOfBirth,
+                    };
+
+                    property.NaturalPersons.Add(firstPerson);
+                    property.NaturalPersons.Add(secondPerson);
+                    await _applicationDBContext.AddAsync(property);
+                    await _applicationDBContext.SaveChangesAsync();
+
+                    _add.IsReturn = false;
+
+                    return RedirectToAction("Index", "Properties");
+                }
+            }
+            catch (FormatException)
+            {
+                ModelState.AddModelError("RoomId", "Введите корректное значение.");
+            }
+            catch (ArgumentNullException)
+            {
+                ModelState.AddModelError("RoomId", "Введите значение.");
+            }
+
+            if (TempData.ContainsKey("IndexPropertiesModel"))
+            {
+                _add.IsReturn = true;
+
+                var serializedModel = TempData["IndexPropertiesModel"] as string;
+                var model = JsonConvert.DeserializeObject<IndexPropertiesModel>(serializedModel);
+
+                var rooms = model.AddProperties.AddFullProperty.Rooms;
+
+                model.AddProperties.AddFullProperty = new AddFullPropertyModel();
+                model.AddProperties.AddSharedProperty = new AddSharedPropertyModel();
+                model.AddProperties.AddJointProperty = _add;
+
+                model.AddProperties.AddFullProperty.Rooms = rooms;
+                model.AddProperties.AddSharedProperty.Rooms = rooms;
+                model.AddProperties.AddJointProperty.Rooms = rooms;
+
+                serializedModel = JsonConvert.SerializeObject(model);
+                TempData["IndexPropertiesModel"] = serializedModel;
+
+                return View("Index", model);
+            }
+
+            _add.IsReturn = true;
+
+            return View("Index", new IndexPropertiesModel
+            {
+                AddProperties = new AddPropertiesModel
+                {
+                    AddJointProperty = _add
+                }
+
+            });
         }
     }
 }
