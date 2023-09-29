0d0a7bff38ba0280cc131807a55fe4505c41a5ab
aa.semenenko <deskri@mail.ru>
2023-09-29 12:44:10 +0400

    Добавлен класс фильтра собственности

--- IS_FHGMOABO/Models/PropertiesModels/IndexPropertiesModel.cs
+++ IS_FHGMOABO/Models/PropertiesModels/IndexPropertiesModel.cs
@@ -7,5 +7,18 @@ namespace IS_FHGMOABO.Models.PropertiesModels
         public AddPropertiesModel AddProperties { get; set; }
 
         public List<Property> Properties { get; set; }
+
+        public PropertiesFilter Filter { get; set; }
+    }
+
+    public class PropertiesFilter
+    {
+        List<House> Houses { get; set; }
+
+        public int HouseId { get; set; }
+
+        public string Room { get; set; }
+
+        public string PropertyType { get; set; }
     }
 }
