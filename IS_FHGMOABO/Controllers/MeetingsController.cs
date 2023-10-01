cde89ea13cbbec8c34477eba3d4e5dafea68b0c2
aa.semenenko <deskri@mail.ru>
2023-10-01 19:18:49 +0400

    Добавлен контроллер собраний

---
+++ IS_FHGMOABO/Controllers/MeetingsController.cs
@@ -0,0 +1,23 @@
+﻿using IS_FHGMOABO.DBConection;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace IS_FHGMOABO.Controllers
+{
+    public class MeetingsController : Controller
+    {
+        private readonly ApplicationDBContext _applicationDBContext;
+
+        public MeetingsController(ApplicationDBContext applicationDBContext)
+        {
+            _applicationDBContext = applicationDBContext;
+        }
+
+        [Authorize]
+        [HttpGet]
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
