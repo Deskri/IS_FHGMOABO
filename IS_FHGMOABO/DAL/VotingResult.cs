795d2f9d4403bf2c30c1b59e5ed6fa37b82f6df3
aa.semenenko <deskri@mail.ru>
2023-11-10 11:38:54 +0400

    Добавлена DAL модель результатов голосования

---
+++ IS_FHGMOABO/DAL/VotingResult.cs
@@ -0,0 +1,45 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace IS_FHGMOABO.DAL
+{
+    [Table("VotingResults")]
+    public class VotingResult
+    {
+        /// <summary>
+        /// Идентификатор результата голосования
+        /// </summary>
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int Id { get; set; }
+
+        /// <summary>
+        /// Внешний ключ для связи с Question
+        /// </summary>
+        [ForeignKey("Meeting")]
+        public int QuestionId { get; set; }
+
+        /// <summary>
+        /// Навигационное свойство для связи с Question
+        /// </summary>
+        [InverseProperty("VotingResults")]
+        public Question? Question { get; set; }
+
+        /// <summary>
+        /// Внешний ключ для связи с Question
+        /// </summary>
+        [ForeignKey("Bulletin")]
+        public int BulletinId { get; set; }
+
+        /// <summary>
+        /// Навигационное свойство для связи с Question
+        /// </summary>
+        [InverseProperty("VotingResults")]
+        public Bulletin? Bulletin { get; set; }
+
+        /// <summary>
+        /// Результат голосования
+        /// </summary>
+        public int? Result { get; set; }
+    }
+}
