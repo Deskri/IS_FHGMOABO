4387a8a65532b96207ac8584ac23e422f011471e
aa.semenenko <deskri@mail.ru>
2023-11-24 11:43:34 +0400

    Создана таблица в бд с результатами по собранию

--- IS_FHGMOABO/DBConection/ApplicationDBContext.cs
+++ IS_FHGMOABO/DBConection/ApplicationDBContext.cs
@@ -22,5 +22,6 @@ namespace IS_FHGMOABO.DBConection
         public DbSet<Question> Questions { get; set; }
         public DbSet<Bulletin> Bulletins { get; set; }
         public DbSet<VotingResult> VotingResults { get; set; }
+        public DbSet<MeetingResult> MeetingResults { get; set; }
     }
 }
