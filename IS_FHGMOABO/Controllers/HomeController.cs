0b51c305a5f4d3707d95f2381eed14cf122f01a4
aa.semenenko <deskri@mail.ru>
2023-11-28 12:25:22 +0400

    Оформлена главная страница, сделаны графики

--- IS_FHGMOABO/Controllers/HomeController.cs
+++ IS_FHGMOABO/Controllers/HomeController.cs
@@ -1,23 +1,65 @@
-﻿using IS_FHGMOABO.Models;
+﻿using IS_FHGMOABO.DAL;
+using IS_FHGMOABO.DBConection;
+using IS_FHGMOABO.Models;
+using IS_FHGMOABO.Models.HomeModels;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using System.Diagnostics;
 
 namespace IS_FHGMOABO.Controllers
 {
     public class HomeController : Controller
     {
-        private readonly ILogger<HomeController> _logger;
+        private readonly ApplicationDBContext _applicationDBContext;
 
-        public HomeController(ILogger<HomeController> logger)
+        public HomeController(ApplicationDBContext applicationDBContext)
         {
-            _logger = logger;
+            _applicationDBContext = applicationDBContext;
         }
 
         [Authorize]
-        public IActionResult Index()
+        public async Task<IActionResult> Index()
         {
-            return View();
+            var meeting = await _applicationDBContext.Meetings.Include(x => x.ArchivalInformationOfMeeting)
+                                                              .Include(x => x.House)
+                                                              .ToListAsync();
+
+            var _currentMeetings = meeting.Where(x => x.Status != "Завершено").OrderBy(x => x.StartDate).ToList();
+
+            var _house = await _applicationDBContext.Houses.Include(x => x.Rooms)
+                                                           .ToListAsync();
+            _house.OrderByDescending(x => x.Id);
+
+            var _adoptedMeetings = meeting.Where(x => x.Status == "Завершено" 
+            && x.ArchivalInformationOfMeeting.TotalAreaHouse/2 <= x.ArchivalInformationOfMeeting.ParticipatingArea).Count();
+            var _unacceptedMeetings = meeting.Where(x => x.Status == "Завершено"
+            && x.ArchivalInformationOfMeeting.TotalAreaHouse / 2 > x.ArchivalInformationOfMeeting.ParticipatingArea).Count();
+
+            var _meetingsDuringTheYears = new List<DateAndCount>();
+            for (int i = 0; i < 3; i++)
+            {
+                var date = DateTime.Now.Year - i;
+                var count = meeting.Where(x => x.StartDate.Year == date).Count();
+                _meetingsDuringTheYears.Add(new DateAndCount()
+                {
+                    Date = date,
+                    Count = count,//ксененок очень любит котеночка
+                });
+            }
+
+            _meetingsDuringTheYears = _meetingsDuringTheYears.OrderBy(x => x.Date).ToList();
+
+            var model = new IndexModel()
+            {
+                СurrentMeetings = _currentMeetings,
+                AdoptedMeetingsCount = _adoptedMeetings,
+                UnacceptedMeetingsCount = _unacceptedMeetings,
+                Houses = _house,
+                MeetingsDuringTheYears = _meetingsDuringTheYears,
+            };
+
+            return View(model);
         }
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
